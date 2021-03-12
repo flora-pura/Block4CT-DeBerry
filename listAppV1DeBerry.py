f0d0ee0c68a8e4c31e9b17c39ef44b175636c96c
Rae <79872916+flora-pura@users.noreply.github.com>
2021-03-12 13:12:49 -0700

    Add files via upload

--- listAppV1DeBerry.py
+++ listAppV1DeBerry.py
@@ -11,18 +11,23 @@ Program Goals:
 myList = []
 
 def mainProgram():
+    #build a while loop here!
     while True:
-    print("Hello, there! Lets work with lists!")
-    print("Choose from the following options. Type a number below!")
-    choice = input("""1. Add to list ,
-2. Return the value at index position!
-3. Exit program.  """)
-    if choice == "1":
-        addToList()
-    elif choice == "2":
-        indexValue()
-    elif choice == "3":
-        break
+        try: 
+            print("Hello, there! Lets work with lists!")
+            print("Choose from the following options. Type a number below!")
+            choice = input("""1. Add to list ,
+2. Return the value at an index,
+3. Random search,
+4. Exit program.  """)
+            if choice == "1":
+                addToList()
+            elif choice == "2":
+                indexValue()
+            elif choice == "3":
+                break
+        except:
+                print("You made a whoopsie!")
 
     #TO ADD: 1. a way to loop the action, 2. a way to quit, 3. think of repetition
 
@@ -35,6 +40,10 @@ def indexValues():
     print("Ohhh! I heard you a particular piece of data!")
     indexPos = input("What index position are you curious about?  ")
     print(myList[int(indexPos)])
+
+def randomSearch():
+    print("RaNDoM SeaRCH!")
+    print(myList [random.randint(0, len (myList)-1)])
 
 #dunder main -> Double Underscore---dunder
 if __name__ == "__main__":
